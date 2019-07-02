3ef7861349469e8272b7a02a690caaf5541165a3
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-07-02 12:58:53 +0300

    2.

--- AmigoSet.java
+++ AmigoSet.java
@@ -1,11 +1,30 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
-public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set {
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+
+    private static final Object PRESENT = new Object(); //это будет наша заглушка.
+    private transient HashMap<E, Object> map; //Список ключей будет нашим сэтом, а вместо значений будем пихать в мапу заглушку PRESENT.
+
+    public AmigoSet() {
+        this.map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        this.map = new HashMap<>((int)Math.max(16, (collection.size()/.75f) + 1));
+        for (E e : collection) {
+            map.put(e, PRESENT);
+        }
+    }
+
+    @Override
+    public boolean add(E e) {
+        int size = map.size();
+        map.put(e, PRESENT);
+        return map.size() > size;
+    }
 
     @Override
     public Iterator iterator() {
